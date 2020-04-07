732a83f411edcd5ba666a1e20b433286f66786e0
Benjamin Köhler <benjaminkoehler89@gmail.com>
2020-04-07 10:59:44 +0200

    disable outpot processing
    usually, a new line '\n' will automatically cause a carriage return '\r' to happen.
    Without output processing, a new line will just stay a new line, so if we want, we need
    to put in a carraige return explicitly.

--- editor.c
+++ editor.c
@@ -16,6 +16,7 @@ void enable_raw_mode() {
 
   struct termios raw = original_terminal_state;
   raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_oflag &= ~(OPOST);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -27,9 +28,9 @@ int main() {
   char c;
   while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
     if(iscntrl(c)){
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } else {
-      printf("%d ('%c')\n", c, c);
+      printf("%d ('%c')\r\n", c, c);
     }
   }
   disable_raw_mode();
