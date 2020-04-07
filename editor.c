f205a89bbb7bd4558d10e0262c00d79d78873452
Benjamin Köhler <benjaminkoehler89@gmail.com>
2020-04-07 11:27:31 +0200

    add some misc flags to be compatible with all terminals

--- editor.c
+++ editor.c
@@ -15,8 +15,10 @@ void enable_raw_mode() {
   atexit(disable_raw_mode);
 
   struct termios raw = original_terminal_state;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_iflag &= ~(ICRNL | IXON);
   raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
